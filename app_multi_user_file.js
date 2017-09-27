bac49cdbbdcb064a753403d870fa321e57842c0c
SeoWonHo <seowonhokr>
2017-09-27 17:29:48 +0900

    20170927

--- app_multi_user_file.js
+++ app_multi_user_file.js
@@ -15,7 +15,7 @@ app.get('/count',function(req,res){
   if(req.session.count){
     req.session.count++;
   }else{
-      req.session.count = 1;
+      req.session.count =  1;
   }
 
   res.send('count : '+  req.session.count );
